b7be91b6e5c885b8ae470c99e00a48239073d1bb
Zizaco <zizaco@gmail.com>
2015-04-06 22:32:43 -0300

    Added Page and Post entities and also added a psr-4 loader to 'src' directory

---
+++ src/Cms/Page.php
@@ -0,0 +1,13 @@
+<?php namespace Blog\Cms;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Page Class
+ *
+ * The Page class is the Entity that represents a cms page.
+ *
+ * @package Blog\Cms
+ */
+class Page extends Model {
+}
---
+++ src/Cms/Post.php
@@ -0,0 +1,15 @@
+<?php namespace Blog\Cms;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Post Class
+ *
+ * The Post class is the Entity that represents a blog post entry.
+ * It extends the Blog\Cms\Page since the blog post have the same behavior
+ * that cms pages have.
+ *
+ * @package Blog\Cms
+ */
+class Post extends Page {
+}

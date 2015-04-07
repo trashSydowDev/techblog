cb280de4da41429a3b162f5c7b75294abaa50e53
Zizaco <zizaco@gmail.com>
2015-04-06 22:47:38 -0300

    Added render method to base controller in order to easly tweak one place when changing the view rendering behavior

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,26 @@ use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
+/**
+ * BaseController Class
+ *
+ * Contains behavior that is common to all controllers
+ */
 abstract class Controller extends BaseController {
 
 	use DispatchesCommands, ValidatesRequests;
+
+    /**
+     * Make a view
+     *
+     * @param  string  $view
+     * @param  array   $data
+     * @param  array   $viewData Data that will be passed to the View only, so it will not be visible in Json responses
+     * @return Illuminate\Support\Contracts\RenderableInterface a renderable View or Response object
+     */
+    protected function render($view, $data = array(), $viewData = array())
+    {
+        return view($view, array_merge($data, $viewData));
+    }
 
 }

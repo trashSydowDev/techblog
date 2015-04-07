5333f7e1ad48c7d754aaf5f1f288f9b0b64bf648
Zizaco <zizaco@gmail.com>
2015-04-06 23:10:26 -0300

    Added redirection methods to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -20,11 +20,41 @@ abstract class Controller extends BaseController {
      * @param  string  $view
      * @param  array   $data
      * @param  array   $viewData Data that will be passed to the View only, so it will not be visible in Json responses
+     *
      * @return Illuminate\Support\Contracts\RenderableInterface a renderable View or Response object
      */
     protected function render($view, $data = array(), $viewData = array())
     {
         return view($view, array_merge($data, $viewData));
+    }
+
+    /**
+     * Create a new redirect response to the given path.
+     *
+     * @param  string  $path
+     * @param  int     $status
+     * @param  array   $headers
+     * @param  bool    $secure
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function goToUrl($path, $status = 302, $headers = array(), $secure = null)
+    {
+        return redirect($path, $status, $headers, $secure);
+    }
+    /**
+     * Create a new redirect response to a controller action.
+     *
+     * @param  string  $action
+     * @param  array   $parameters
+     * @param  int     $status
+     * @param  array   $headers
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function goToAction($action, $parameters = array(), $status = 302, $headers = array())
+    {
+        return redirect()->action($action, $parameters, $status, $headers);
     }
 
 }

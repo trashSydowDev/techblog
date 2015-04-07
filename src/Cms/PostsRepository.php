423c36d52559806ce85827a8f9828a91e7b7fac9
Zizaco <zizaco@gmail.com>
2015-04-06 22:58:14 -0300

    Added PostsRepository

---
+++ src/Cms/PostsRepository.php
@@ -0,0 +1,22 @@
+<?php
+namespace Blog\Cms;
+
+use App;
+
+/**
+ * Class PageRepository
+ *
+ * The Blog\Cms\PageRepository abstracts all the database queries regarding
+ * Posts.
+ *
+ * @package  Blog\Cms
+ */
+class PostRepository extends PageRepository
+{
+    /**
+     * The domain object class that is going to query for using
+     * Eloquent methods.
+     * @var string
+     */
+    public $domainObject = 'Blog\Cms\Post';
+}

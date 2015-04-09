27f8729d8798becc906978d198990da11d41b516
Zizaco <zizaco@gmail.com>
2015-04-08 21:45:15 -0300

    Added lang files for resources and basic dialogs

---
+++ resources/lang/en/resources.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+    'Post' => 'Blog Post',
+    'Page' => 'Page',
+    'User' => 'User',
+
+    'attributes' => array(
+        'Page' => array(
+            'title'        => 'Title',
+            'slug'         => 'Slug',
+            'lean_content' => 'Short Description',
+            'content'      => 'Content',
+            'author'       => 'Author',
+            'created_at'   => 'Created at',
+        ),
+        'Post' => array(
+            'title'        => 'Title',
+            'slug'         => 'Slug',
+            'lean_content' => 'Short Description',
+            'content'      => 'Content',
+            'author'       => 'Author',
+            'created_at'   => 'Created at',
+        )
+    )
+);

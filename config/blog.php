1490bf3b130b9c4ea82029b09bd488176be36ddf
Zizaco <zizaco@gmail.com>
2015-04-13 22:46:46 -0300

    Added michelf/php-markdown as dependency to render markdown

---
+++ config/blog.php
@@ -0,0 +1,26 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Name of the Blog
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'blogname' => "Leroy Merlin Techblog",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Discuss Name
+    |--------------------------------------------------------------------------
+    |
+    | Tells the Disqus service your forum's shortname, which is the unique
+    | identifier for your website as registered on Disqus. If null, the
+    | Disqus embed will not load.
+    |
+    */
+
+    'discuss_name' => 'leroymerlin-techblog',
+);

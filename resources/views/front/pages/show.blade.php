574ba5163cba2bab73fef07d8152dc146750e93e
Zizaco <zizaco@gmail.com>
2015-04-13 22:48:09 -0300

    Added Cms\Presenter and basic front-end views

---
+++ resources/views/front/pages/show.blade.php
@@ -0,0 +1,7 @@
+@extends('front.templates.main')
+
+@section('content')
+    <div class="l-block-1">
+        {!! $pagePresenter->display() !!}
+    </div>
+@stop
---
+++ src/Cms/Presenter.php
@@ -0,0 +1,55 @@
+<?php
+namespace Blog\Cms;
+
+use App;
+use View;
+
+/**
+ * Class Presenter
+ *
+ * The Presenter service has the responsability of rendering the given Page/Post
+ * domain object.
+ *
+ * @package  Blog\Cms
+ */
+class Presenter
+{
+    /**
+     * The page to be presented
+     * @var Page
+     */
+    public $instance = null;
+
+    /**
+     * Sets the instance that is going to be presented.
+     * @param  Page   $instance Page or post to be presented
+     * @return void
+     */
+    public function setInstance(Page $instance)
+    {
+        $this->instance = $instance;
+    }
+
+    /**
+     * Displays the page instance
+     * @return Illuminate\View\View A view displaying the page
+     */
+    public function display()
+    {
+        $mdParser = App::make('Michelf\MarkdownExtra');
+
+        $htmlContent = $mdParser->transform($this->instance->content);
+
+        $viewVars = [
+            'page' => $this->instance,
+            'htmlContent' => $htmlContent
+        ];
+
+        // Due to the polymorphy
+        if($this->instance instanceOf Post) {
+            return View::make('front.posts._display', $viewVars);
+        } else {
+            return View::make('front.pages._display', $viewVars);
+        }
+    }
+}

5b90572c07adbbf179010e542a8773823a080c1b
Zizaco <zizaco@gmail.com>
2015-07-18 09:32:45 -0300

    Added "author-card" styling on the top of the posts

--- resources/views/front/posts/_display.blade.php
+++ resources/views/front/posts/_display.blade.php
@@ -2,8 +2,8 @@
     <h1 itemprop="name headline">
         {{ $page->title }}
     </h1>
-    <div>
-        por
+    <div class="author-card">
+        Por
         <span itemprop="author" itemscope="" itemtype="http://schema.org/Person">
             <span itemprop="name">
                 <a href="{{ $author->website }}" itemprop="url" rel="author">
@@ -11,7 +11,7 @@
                 </a>
             </span>
         </span>
-        em
+        -
         <span datetime="{{ date('c', strtotime($page->created_at)) }}" itemprop="datePublished">
             {{ date('M d, Y', strtotime($page->created_at)) }}
         </span>

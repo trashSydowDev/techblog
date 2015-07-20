979a965f2e757f68673d0e7b4201666ec0f0d067
Zizaco <zizaco@gmail.com>
2015-07-20 06:41:49 -0300

    Updated feed RSS to match best practices

--- resources/views/front/posts/feed.blade.php
+++ resources/views/front/posts/feed.blade.php
@@ -1,7 +1,8 @@
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>{{ Config::get('blog.blogname') }}</title>
-    <link>{{ URL::to('/') }}</link>
+    <link>{{ url('/') }}</link>
+    <atom:link href="{{ action('Frontend\CmsController@feedPosts') }}" rel="self" type="application/rss+xml" />
     @if (Config::get('blog.blogdesc'))
       <description>{{ Config::get('blog.blogdesc') }}</description>
     @else
@@ -12,6 +13,7 @@
       <item>
         <title><![CDATA[{{ $post->title }}]]></title>
         <link>{{ action('Frontend\CmsController@showPost', ['slug'=>$post->slug]) }}</link>
+        <guid>{{ action('Frontend\CmsController@showPost', ['slug'=>$post->slug]) }}</link>
         <description><![CDATA[{{ $post->lean_content }}]]></description>
         <pubDate>{{ date('r', strtotime($post->created_at)) }}</pubDate>
       </item>
